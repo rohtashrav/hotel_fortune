3b34292958c6532e92d392df736b88a3cbbe5f9d
hp <simranc992@gmail.com>
2020-01-15 16:40:25 +0530

    uncomment line @simran

--- backend_panel_app/models.py
+++ backend_panel_app/models.py
@@ -5,8 +5,9 @@ class UserRole(models.Model):
     role_id = models.AutoField(primary_key=True)
     role_name = models.CharField(max_length=255, unique=True, null=True, blank=True)
 
-    # def __str__(self):
-    #     return self.role_name
+    def __str__(self):
+        return self.role_name
+
 
 class RoleDetails(models.Model):
     role_id = models.ForeignKey(UserRole, on_delete=models.CASCADE)
