34920660e027573dbcca5ad1e1ef9bfc174eefad
rohtashrav <logout.me.world@gmail.com>
2020-01-15 16:06:55 +0530

    add UserRole model in file @rohtash

--- backend_panel_app/models.py
+++ backend_panel_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 
+class UserRole(models):
+    role_id = models.AutoField(primary_key=True)
+    role_name = models.CharField(max_length=255, unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return self.role_name
+    
