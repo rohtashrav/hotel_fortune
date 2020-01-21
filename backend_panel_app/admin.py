30202f2ad868c7afbe93076a83cb1d421e83a8a4
hp <simranc992@gmail.com>
2020-01-21 15:09:59 +0530

    login fun @simran

--- backend_panel_app/admin.py
+++ backend_panel_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from backend_panel_app.models import UserRole
+from backend_panel_app.models import UserRole, RoleDetails
 
 
 admin.site.register(UserRole)
+admin.site.register(RoleDetails)
 
--- backend_panel_app/views.py
+++ backend_panel_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
+from backend_panel_app.models import RoleDetails
+from django.db.models import Q
+from django.contrib.auth.hashers import check_password
 
-# Create your views here.
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST["email"]
+        password = request.POST["password"]
+        try:
+            data = RoleDetails.objects.get(email=email)
+            u_password = data.password
+            if check_password(password, u_password):
+                pass
+            else:
+                return HttpResponse("Invalid Password")
+        except:
+            return HttpResponse("Data not found")
